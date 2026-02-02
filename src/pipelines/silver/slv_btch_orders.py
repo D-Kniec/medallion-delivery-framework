770a10b25a26c0e5ad229edbb5434bad96604484
Dominik Kniec <d.kniec.github@gmail.com>
2026-02-02 12:59:52 +0100

    refactor: migrate orders to pipelines & update to batch

--- src/pipelines/silver/slv_btch_orders.py
+++ src/pipelines/silver/slv_btch_orders.py
@@ -1,18 +1,21 @@
 import os
+import sys
+from pathlib import Path
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 
-current_script_path = os.path.abspath(__file__)
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_script_path)))
+current_script_path = Path(__file__).resolve()
+project_root = current_script_path.parents[3]
 
-input_path = os.path.join(project_root, "src", "bronze", "orders")
-output_path = os.path.join(project_root, "src", "silver", "orders")
-checkpoint_path = os.path.join(project_root, "chk", "orders_bronze_to_silver")
+input_path = str(project_root / "data" / "bronze" / "orders")
+output_path = str(project_root / "data" / "silver" / "orders")
+checkpoint_path = str(project_root / "data" / "checkpoints" / "orders_bronze_to_silver_v2")
 
 spark = SparkSession.builder \
     .appName("PizzaDelivery-Orders-BronzeToSilver") \
     .config("spark.sql.streaming.schemaInference", "false") \
+    .config("spark.sql.streaming.fileSource.cleanSource", "archive") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
@@ -30,7 +33,8 @@ orders_schema = StructType([
 
 raw_orders = spark.readStream \
     .schema(orders_schema) \
-    .option("maxFilesPerTrigger", 1000) \
+    .option("maxFilesPerTrigger", 10000) \
+    .option("cleanSource", "delete") \
     .json(input_path)
 
 silver_orders = raw_orders \
@@ -40,12 +44,12 @@ silver_orders = raw_orders \
     .filter(F.col("gross_revenue") >= 0)
 
 query = silver_orders \
-    .coalesce(1) \
     .writeStream \
     .outputMode("append") \
     .format("parquet") \
     .option("path", output_path) \
     .option("checkpointLocation", checkpoint_path) \
+    .option("maxRecordsPerFile", 100000) \
     .partitionBy("order_date") \
     .trigger(availableNow=True) \
     .start()
